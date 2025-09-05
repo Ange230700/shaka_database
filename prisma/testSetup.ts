3b11ee898894c7924e9ffd3153464ebfba288fb7
Ange230700 <kouakouangeericstephane@gmail.com>
2025-09-06 01:54:47 +0200

    chore: do stuff

--- prisma/testSetup.ts
+++ prisma/testSetup.ts
@@ -4,7 +4,8 @@ import { execSync } from "node:child_process";
 
 export default async function () {
   try {
-    execSync("npm run prisma:migrate:reset:force", { stdio: "inherit" });
+    execSync("npm run prisma:db:push", { stdio: "inherit" });
+    execSync("npm run prisma:db:seed", { stdio: "inherit" });
   } catch (err) {
     console.error("Error in Jest globalSetup:", err);
     throw err;
